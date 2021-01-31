b17c64958b4327b75ab5aea61d5fc5f35c422989
Philipe Go <philipe.ng@protonmail.com>
2021-01-30 19:20:29 -0500

    Improved player rotation and fixed health.cs bug

--- Assets/Scripts/Skullatron6000/BossTrigger.cs
+++ Assets/Scripts/Skullatron6000/BossTrigger.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossTrigger : MonoBehaviour {
+public class BossTrigger : MonoBehaviour
+{
     void Start() { }
 
     void Update() { }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
+#if UNITY_EDITOR
         Debug.Log("Boss Fight Starts");
+#endif
+
         GameObject.FindObjectOfType<SkullatronHandController>().isBossFightStarted = true;
     }
 }
