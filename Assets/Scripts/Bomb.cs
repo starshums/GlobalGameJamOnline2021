620c7261860b1ac0e96672c76df8d4d67a689c54
Nishant Khalate <nishantkhalate10@gmail.com>
2021-01-31 16:22:54 +0530

    Minor tweaks

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -18,7 +18,7 @@ public class Bomb : MonoBehaviour
     GameObject explosionEffect;
 
 
-    public float radius = 7f;
+    public float radius = 8f;
     public float explosionForce = 500f;
 
     public float delay = 3f;
